44836ccad9ecd9d647d23a7fb074b17e67251b64
Tom <tom@contextfree.info>
2025-01-23 07:12:22 -0700

    Just capture on click

--- world.gd
+++ world.gd
@@ -2,7 +2,12 @@ extends Node3D
 
 func _ready() -> void:
 	Globals.player = $Player
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	# Capture also on click because web.
+	if event is InputEventMouseButton and event.pressed:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _process(_delta: float) -> void:
-	# Continue to ask for capture for web export after-click reasons.
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	pass
