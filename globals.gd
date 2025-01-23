6b1b3359e049a147ecdf79b2fa56a6431da760d0
Tom <tom@contextfree.info>
2025-01-23 06:18:06 -0700

    Face the right way

--- globals.gd
+++ globals.gd
@@ -1,13 +1,14 @@
 extends Node
 
 var bubble_size := 49
+var player: Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func keep_in_bubble(position: Vector3) -> Vector3:
