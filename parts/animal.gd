3be736ca0b8b1f6a11aede638944f0d734288043
Tom <tom@contextfree.info>
2025-01-22 20:54:55 -0700

    Add and move animals somewhat (#3)
    * Add javelina; reduce res; improve noise
    * Add coyote & roadrunner
    * Move animals somewhat

---
+++ parts/animal.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+var change_delay: float
+var heading: Vector2
+@onready var sprite: AnimatedSprite3D = $AnimatedSprite3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pick_heading()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	change_delay -= delta
+	if change_delay < 0:
+		pick_heading()
+	position = Globals.keep_in_bubble(position)
+	var move := heading #* delta
+	velocity = Vector3(move.x, velocity.y, move.y)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	move_and_slide()
+
+func pick_heading() -> void:
+	change_delay = randf_range(1.0, 5.0)
+	if randf() < 0.3:
+		sprite.stop()
+		heading = Vector2.ZERO
+	else:
+		sprite.play()
+		heading = 2 * (Vector2(randf(), randf()) - Vector2.ONE * 0.5)
--- parts/player.gd
+++ parts/player.gd
@@ -1,33 +1,27 @@
 extends CharacterBody3D
 
-
-var bubble_size := 49
 const SPEED := 5.0
 const JUMP_VELOCITY := 4.5
 @export var mouse_sensitivity := 5e-3
+@onready var camera: Camera3D = $Camera3D
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-
 func _process(delta: float) -> void:
 	var mouse_delta := Input.get_last_mouse_velocity()
 	rotate_y(-mouse_delta.x * mouse_sensitivity * delta)
-	#downcast.
-	var distance := position.length()
-	if distance > bubble_size:
-		position = position.normalized() * bubble_size
-
+	camera.rotate_x(-mouse_delta.y * mouse_sensitivity * delta)
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -80, 80)
+	position = Globals.keep_in_bubble(position)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -38,5 +32,4 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
