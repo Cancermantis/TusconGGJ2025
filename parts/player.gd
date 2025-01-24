871780e71eb1d83450e09badbcb3679c812f6062
Andy Simons <andylsimons@gmail.com>
2025-01-24 02:52:19 -0500

    Added sneaking and sprinting keyboard inputs Implemented sneaking and sprinting functionality on player character, allowing keyboard control of movement speed Player's current velocity now drives its SoundBroadcaster's base_volume value Set the default clear color for all scenes to black

--- parts/player.gd
+++ parts/player.gd
@@ -1,13 +1,34 @@
 class_name Player
 extends CharacterBody3D
 
+const SPEED_QUIET := 1.0
 const SPEED := 5.0
+const SPEED_SPRINT := 10.0
 const JUMP_VELOCITY := 4.5
 @export var mouse_sensitivity := 5e-3
 @onready var camera: Camera3D = $Camera3D
+@onready var broadcaster: SoundBroadcaster = $SoundBroadcaster
+
+var stealth: bool = false
+var sprinting: bool = false
+var current_max_speed = SPEED
 
 func _ready() -> void:
 	pass
+
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("stealth") && !sprinting):
+		stealth = true
+		current_max_speed = SPEED_QUIET
+	elif(event.is_action_released("stealth") && stealth):
+		stealth = false
+		current_max_speed = SPEED
+	if(event.is_action_pressed("sprint") && !stealth):
+		sprinting = true
+		current_max_speed = SPEED_SPRINT
+	elif(event.is_action_released("sprint") && sprinting):
+		sprinting = false
+		current_max_speed = SPEED
 
 func _process(delta: float) -> void:
 	var mouse_delta := Input.get_last_mouse_velocity()
@@ -28,9 +49,10 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_max_speed
+		velocity.z = direction.z * current_max_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_max_speed)
+		velocity.z = move_toward(velocity.z, 0, current_max_speed)
+	broadcaster.base_volume = velocity.length() / SPEED
 	move_and_slide()
